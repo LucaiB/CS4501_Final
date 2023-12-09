92c8528b6a5ab5a8a868b9d882edc705dc4c31b0
Lucas Banerji <76792861+LucaiB@users.noreply.github.com>
2023-12-09 15:37:09 -0500

    Update environmentSetup.sh

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -1,31 +1,25 @@
 #!/usr/bin/env bash
 
-# Install necessary python packages
+# Install necessary packages
 echo "Installing required packages..."
-sudo yum install gcc make glibc-static bzip2 -y  # Python 3.8 and pip should already be available in CloudShell
+sudo yum install -y gcc make glibc-static bzip2 gnupg2
 echo "Required packages installed."
 
 # Set up virtual environment
-pip3 install virtualenv  # Use pip3 as Python 3.8 is default in CloudShell
+pip3 install virtualenv
 virtualenv python_env
 source python_env/bin/activate
 pip3 install python-gnupg
 deactivate
-rm -rf ./python_env/bin
-mkdir ./python_env/lib_package
-mv python_env/lib64/python3.8/site-packages/* ./python_env/lib_package/
 
-# Download and build GPG binary from source
-wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-1.4.23.tar.bz2
-tar xjf gnupg-1.4.23.tar.bz2
-cd gnupg-1.4.23
-./configure
-make CFLAGS='-static'
-cp g10/gpg ../python_env/lib_package
-cd ../python_env
-chmod o+x lib_package/gpg
+# Copy GnuPG binary and Python packages to a directory for the Lambda layer
+mkdir -p lambda_layer/python
+cp -r python_env/lib/python3.9/site-packages/* lambda_layer/python/
+cp /usr/bin/gpg lambda_layer/python/
 
 # Package for AWS Lambda Layer
-zip -r lambdaLayer.zip lib_package/
-aws lambda publish-layer-version --layer-name python-gnupg --description "Python-GNUPG Module and GPG Binary" --zip-file fileb://lambdaLayer.zip --compatible-runtimes python3.8
+cd lambda_layer
+zip -r ../lambdaLayer.zip python/
+cd ..
+aws lambda publish-layer-version --layer-name python-gnupg --description "Python-GNUPG Module and GPG Binary" --zip-file fileb://lambdaLayer.zip --compatible-runtimes python3.9
 echo "Lambda layer created successfully."
