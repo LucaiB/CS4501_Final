2a8d1ab309928cbd106df34d81f4a9e539549f47
Lucas Banerji <76792861+LucaiB@users.noreply.github.com>
2023-12-09 15:31:17 -0500

    Update environmentSetup.sh

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
-# Installing necessary python packages
-echo Installing required packages...
-cd /home/cloudshell-user
-sudo amazon-linux-extras enable python3.8
-sudo yum install python3.8 gcc make glibc-static bzip2 pip -y
-echo Required packages installed. 
-pip3.8 install virtualenv
-virtualenv python
-cd python
-. bin/activate
-pip3.8 install python-gnupg
-deactivate
-rm -rf ./bin
-mkdir ./python
-mv lib ./python/
+# Install necessary python packages
+echo "Installing required packages..."
+sudo yum install gcc make glibc-static bzip2 -y  # Python 3.8 and pip should already be available in CloudShell
+echo "Required packages installed."
 
-# Downloading and building GPG binary from source
+# Set up virtual environment
+pip3 install virtualenv  # Use pip3 as Python 3.8 is default in CloudShell
+virtualenv python_env
+source python_env/bin/activate
+pip3 install python-gnupg
+deactivate
+rm -rf ./python_env/bin
+mkdir ./python_env/lib_package
+mv python_env/lib64/python3.8/site-packages/* ./python_env/lib_package/
+
+# Download and build GPG binary from source
 wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-1.4.23.tar.bz2
 tar xjf gnupg-1.4.23.tar.bz2
-cd gnupg-1.4.23.tar.bz2
-/bin/bash /home/cloudshell-user/python/gnupg-1.4.23/configure
-make CLFAGS='-static'
-cp g10/gpg /home/cloudshell-user/python/python
-cd /home/cloudshell-user/python/python
-chmod o+x gpg
-cd ..
-zip -r lambdaLayer.zip python/
+cd gnupg-1.4.23
+./configure
+make CFLAGS='-static'
+cp g10/gpg ../python_env/lib_package
+cd ../python_env
+chmod o+x lib_package/gpg
+
+# Package for AWS Lambda Layer
+zip -r lambdaLayer.zip lib_package/
 aws lambda publish-layer-version --layer-name python-gnupg --description "Python-GNUPG Module and GPG Binary" --zip-file fileb://lambdaLayer.zip --compatible-runtimes python3.8
-echo Lambda layer created successfully.
+echo "Lambda layer created successfully."
