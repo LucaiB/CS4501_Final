3525623b0f8b462d032299d0faef71070f5f01a2
Lucas Banerji <76792861+LucaiB@users.noreply.github.com>
2023-12-09 15:41:40 -0500

    Update environmentSetup.sh

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-# Install necessary packages
-echo "Installing required packages..."
-sudo yum install -y gcc make glibc-static bzip2 gnupg2
+# Check if gnupg2 is already installed; if not, install it
+if ! command -v gpg > /dev/null 2>&1; then
+    echo "Installing GnuPG..."
+    sudo yum install -y gnupg2
+else
+    echo "GnuPG is already installed."
+fi
+
+echo "Installing other required packages..."
+sudo yum install -y gcc make glibc-static bzip2
 echo "Required packages installed."
 
 # Set up virtual environment
@@ -12,10 +19,13 @@ source python_env/bin/activate
 pip3 install python-gnupg
 deactivate
 
-# Copy GnuPG binary and Python packages to a directory for the Lambda layer
+# Prepare directory for the Lambda layer
 mkdir -p lambda_layer/python
 cp -r python_env/lib/python3.9/site-packages/* lambda_layer/python/
-cp /usr/bin/gpg lambda_layer/python/
+# Check if GnuPG binary is available and copy it
+if [ -f "/usr/bin/gpg" ]; then
+    cp /usr/bin/gpg lambda_layer/python/
+fi
 
 # Package for AWS Lambda Layer
 cd lambda_layer
